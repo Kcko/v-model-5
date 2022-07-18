1687919c4536c12140b0f9447fbff70bd5dfdec7
Kcko <admin@rjwebdesign.cz>
2022-07-18 14:18:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dnd5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dnd5)
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input v-model="a" />
+    <input v-model="b" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      a: 5,
+    };
+  },
+  computed: {
+    b: {
+      get() {
+        return parseInt(this.a) + 1;
+      },
+      set(v) {
+        this.a = v - 1;
+      },
+    },
+  },
+  methods: {},
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
